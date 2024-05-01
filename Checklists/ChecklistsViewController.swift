ded408e751898bdbaeb159da3a8d81c231a5354c
tanaka <tanaka.mawoyo@yahoo.com>
2024-05-01 10:58:29 +0200

    Add tableView Data Source

--- Checklists/ChecklistsViewController.swift
+++ Checklists/ChecklistsViewController.swift
@@ -13,6 +13,16 @@ class ChecklistsViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+     //MARK  : - TableView Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 
 
 }
